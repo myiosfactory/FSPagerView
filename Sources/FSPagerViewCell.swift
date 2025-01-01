9272644a99147a664a3b13c5620e524741b50ca6
Maulik Bhuptani <my.iosfactory@gmail.com>
2025-01-01 19:13:34 +0530

    Update FSPagerViewCell.swift

--- Sources/FSPagerViewCell.swift
+++ Sources/FSPagerViewCell.swift
@@ -48,7 +48,8 @@ open class FSPagerViewCell: UICollectionViewCell {
     fileprivate weak var _imageView: UIImageView?
     
     fileprivate let kvoContext = UnsafeMutableRawPointer(bitPattern: 0)
-    fileprivate let selectionColor = UIColor(white: 0.2, alpha: 0.2)
+    // fileprivate let selectionColor = UIColor(white: 0.2, alpha: 0.2)
+    fileprivate let selectionColor = UIColor.clear
     
     fileprivate weak var _selectedForegroundView: UIView?
     fileprivate var selectedForegroundView: UIView? {
